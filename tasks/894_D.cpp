90ee041c02ca2fcbd075e827901f39d92b3f79b7
andryweru <andryweru@Ubuntu22.10.myguest.virtualbox.org>
2023-09-27 00:59:41 +0300

    changed Makefile, 894_D.cpp

--- tasks/894_D.cpp
+++ tasks/894_D.cpp
@@ -24,8 +24,9 @@ int main() {
 unsigned long long min(unsigned long long kinds) {
 
   unsigned long long res = (1 + sqrt(1 + 8 * kinds)) / 2;
-  if (res * res - res < 2 * kinds)
-    ++res;
+  if (res * res - res > 2 * kinds)
+    --res;
+  res += kinds - (res * res - res)/2;
 
   return res;
 }
