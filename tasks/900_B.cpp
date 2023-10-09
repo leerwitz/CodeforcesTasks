edd183c4ce6bf8c7f091874c1ad7ebb925da2bf3
andryweru <andryweru@Ubuntu22.10.myguest.virtualbox.org>
2023-10-09 22:41:36 +0300

    clang-format

--- tasks/900_B.cpp
+++ tasks/900_B.cpp
@@ -1,44 +1,34 @@
-#include <iostream>
 #include <math.h>
-#include <vector>
 
+#include <iostream>
+#include <vector>
 
 std::vector<int> input_vector(long unsigned int size_of_vector);
 
-
-std::vector<int> input_vector(long unsigned int size_of_vector){
-    std:: vector<int> res = std::vector<int>(size_of_vector);
+std::vector<int> input_vector(long unsigned int size_of_vector) {
+    std::vector<int> res = std::vector<int>(size_of_vector);
     res[0] = 1;
     res[1] = 3;
-    for (long unsigned int i = 2; i < size_of_vector; i++)
-    {
+    for (long unsigned int i = 2; i < size_of_vector; i++) {
         res[i] = res[i - 1] + 1;
-        if(3 * res[i] % (res[i -1] + res[i -1])  == 0)
-        ++res[i];
-        
+        if (3 * res[i] % (res[i - 1] + res[i - 1]) == 0) ++res[i];
     }
 
     return res;
 }
 
-
-
-
-int main(){
+int main() {
     long unsigned int size_of_vector, number_of_inputs;
-    
+
     std::cin >> number_of_inputs;
-    for (long unsigned int i = 0; i < number_of_inputs; i++)
-    {
-        std:: cin >> size_of_vector;
+    for (long unsigned int i = 0; i < number_of_inputs; i++) {
+        std::cin >> size_of_vector;
         std::vector<int> arr = input_vector(size_of_vector);
-        for (long unsigned int j = 0; j < size_of_vector; j++)
-        {
+        for (long unsigned int j = 0; j < size_of_vector; j++) {
             std::cout << arr[j] << " ";
         }
         std::cout << '\n';
-
     }
-    
+
     return 0;
 }
--- tasks/900_D.cpp
+++ tasks/900_D.cpp
@@ -1,103 +1,85 @@
-#include <iostream>
 #include <math.h>
-#include <vector>
+
+#include <algorithm>
+#include <iostream>
 #include <string>
+#include <vector>
 
 void swap(char &a, char &b);
-void substring_reversal(std::string &string, int first, int last, std::vector<std::vector<int>> operation_description,int interval_index);
+void substring_reversal(std::string &string, int first, int last, std::vector<std::vector<int>> operation_description, int interval_index);
 std::vector<std::vector<int>> getMatrix_of_operations(int number_of_operations, std::vector<int> array_l, std::vector<int> array_r);
 
-
-template<typename type>
-std::vector<type> input_array(int size){
+template <typename type>
+std::vector<type> input_array(int size) {
     std::vector<type> res = std::vector<type>(size);
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         std::cin >> res[i];
     }
-    
+
     return res;
 }
 
-
-
-
-void swap(char &a, char &b){
+void swap(char &a, char &b) {
     char c = a;
     a = b;
     b = c;
 }
 
-std::vector<std::vector<int>> getMatrix_of_operations(int number_of_operations, std::vector<int> array_l, std::vector<int> array_r){
-    std::vector<std::vector<int>> res =std::vector<std::vector<int>>(array_l.size());
+std::vector<std::vector<int>> getMatrix_of_operations(int number_of_operations, std::vector<int> array_l, std::vector<int> array_r) {
+    std::vector<std::vector<int>> res = std::vector<std::vector<int>>(array_l.size());
     int current_operation;
-    for (int i = 0; i < number_of_operations; ++i)
-    {
+    for (int i = 0; i < number_of_operations; ++i) {
         std::cin >> current_operation;
-        for (size_t j = 0; j < res.size(); ++j)
-        {
-            if(current_operation >= array_l[j] && current_operation <= array_r[j]){
-                res[j].push_back(current_operation);
+        for (size_t j = 0; j < res.size(); ++j) {
+            if (current_operation >= array_l[j] && current_operation <= array_r[j]) {
+                if (current_operation > (array_l[j] + array_r[j]) / 2)
+                    res[j].push_back(array_l[j] + array_r[j] - current_operation);
+                else
+                    res[j].push_back(current_operation);
                 break;
             }
         }
-        
     }
+
+    for (size_t i = 0; i < res.size(); i++) {
+        std::sort(begin(res[i]), end(res[i]));
+    }
+
     return res;
-    
 }
 
-void substring_reversal(std::string &string, int first, int last, std::vector<std::vector<int>> operation_description, int interval_index){
-std::vector<int> number_of_operations_per_index = std::vector<int>((last - first)/2 + 1, 0); 
-int current_operation;
-    for (size_t i = 0; i < operation_description[interval_index].size(); i++)
-    {
-        current_operation = operation_description[interval_index][i] - 1;
-
-            if (current_operation > (first + last)/2 )
-                current_operation = last + first - operation_description[interval_index][i] + 1;
-            for (int j = 0; j < (last - first)/2 + 1; ++j)
-            {
-                if(current_operation <=  first + j)
-                    ++number_of_operations_per_index[j];
-            }
-            
+void substring_reversal(std::string &string, int first, int last, std::vector<std::vector<int>> operation_description, int interval_index) {
+    int current_operation, end_index, begin_index;
+    for (size_t i = 0; i < operation_description[interval_index].size(); i += 2) {
+        if (i != operation_description[interval_index].size() - 1)
+            end_index = operation_description[interval_index][i + 1] - 1;
+        else
+            end_index = (last + first) / 2 + 1;
+        begin_index = operation_description[interval_index][i] - 1;
+        for (int j = begin_index; j < end_index; ++j) {
+            swap(string[j], string[last - j + first]);
+        }
     }
-
-   for (size_t j = 0; j < number_of_operations_per_index.size(); ++j){
-    if(number_of_operations_per_index[j] % 2 == 1){
-        swap(string[first + j], string[last - j]);
-    }
-   }
 }
 
-int main(){
+int main() {
+    int number_of_inputs, array_size, number_of_operations, string_size;
 
-int number_of_inputs, array_size, number_of_operations, string_size;
-
-
-std::cin >> number_of_inputs;
-for (int i = 0; i < number_of_inputs; i++)
-{
-    std::cin >> string_size >> array_size;
-    std::string string;
-    std::cin >> string;
-    std::vector<int> array_l =  input_array<int>(array_size);
-    std::vector<int> array_r =  input_array<int>(array_size);
-    std::cin >> number_of_operations;
-    std::vector<std::vector<int>> operation_description = getMatrix_of_operations(number_of_operations,array_l,array_r);
-        for (int j = 0; j < array_size; ++j)
-        {
+    std::cin >> number_of_inputs;
+    for (int i = 0; i < number_of_inputs; i++) {
+        std::cin >> string_size >> array_size;
+        std::string string;
+        std::cin >> string;
+        std::vector<int> array_l = input_array<int>(array_size);
+        std::vector<int> array_r = input_array<int>(array_size);
+        std::cin >> number_of_operations;
+        std::vector<std::vector<int>> operation_description = getMatrix_of_operations(number_of_operations, array_l, array_r);
+        for (int j = 0; j < array_size; ++j) {
             substring_reversal(string, array_l[j] - 1, array_r[j] - 1, operation_description, j);
         }
-        
-    
-    
-    std::cout << string << '\n';
-    
-    
-}
 
+        std::cout << string << '\n';
+    }
 
     return 0;
 }
