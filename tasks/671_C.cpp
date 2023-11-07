37cbd161682a3d48b306b85d6ae80a1d0f056164
andryweru <andryweru@Ubuntu22.10.myguest.virtualbox.org>
2023-11-07 11:02:44 +0300

    complete 671_C

--- tasks/671_C.cpp
+++ tasks/671_C.cpp
@@ -2,14 +2,14 @@
 
 #include <iostream>
 #include <vector>
+int max_addition(int current_rating, int killjoy_rating);
+int max_subtraction(int current_rating, int killjoy_rating);
+int GetNumberOfSteps(std::vector<int> &array_rating_healthy, std::vector<int> &array_rating_infected, int killjoy_rating);
+void GetArrayHealthyAccounts(std::vector<int> &array_rating_healthy, std::vector<int> &array_rating_infected, int killjoy_rating, int &amount_rating_infected, int old_size);
 
 int max_addition(int current_rating, int killjoy_rating) { return 4000 - (killjoy_rating - current_rating); }
 
-int max_addition(std::vector<int> array_infected, int amount_infected, int old_size) { return 4000 * old_size - amount_infected; }
-
 int max_subtraction(int current_rating, int killjoy_rating) { return -4000 - (killjoy_rating - current_rating); }
-
-int max_subtraction(std::vector<int> array_infected, int amount_infected, int old_size) { return -4000 * old_size - amount_infected; }
 
 void GetArrayHealthyAccounts(std::vector<int> &array_rating_healthy, std::vector<int> &array_rating_infected, int killjoy_rating, int &amount_rating_infected, int old_size) {
     std::vector<int> result = std::vector<int>();
@@ -19,56 +19,7 @@ void GetArrayHealthyAccounts(std::vector<int> &array_rating_healthy, std::vector
         amount_rating_healthy += array_rating_healthy[i];
     }
 
-    if (amount_rating_healthy < 0) {
-        if (amount_rating_healthy + max_addition(array_rating_infected, amount_rating_infected, old_size) < 0) {
-            amount_rating_healthy += max_addition(array_rating_infected, amount_rating_infected, old_size);
-            amount_rating_infected += max_addition(array_rating_infected, amount_rating_infected, old_size);
-        } else {
-            amount_rating_infected -= amount_rating_healthy;
-            amount_rating_healthy = 0;
-        }
-    } else {
-        if (amount_rating_healthy + max_subtraction(array_rating_infected, amount_rating_infected, old_size) > 0) {
-            amount_rating_healthy += max_subtraction(array_rating_infected, amount_rating_infected, old_size);
-            amount_rating_infected += max_subtraction(array_rating_infected, amount_rating_infected, old_size);
-        } else {
-            amount_rating_infected -= amount_rating_healthy;
-            amount_rating_healthy = 0;
-        }
-    }
-
-    if (amount_rating_healthy == 0)
-        array_rating_healthy.clear();
-    else {
-        while (amount_rating_healthy != 0) {
-            amount_rating_healthy -= array_rating_healthy[j];
-
-            if (amount_rating_healthy < 0) {
-                if (amount_rating_healthy + max_addition(array_rating_healthy[j], killjoy_rating) < 0) {
-                    amount_rating_healthy += max_addition(array_rating_healthy[j], killjoy_rating);
-                    array_rating_healthy[j] += max_addition(array_rating_healthy[j], killjoy_rating);
-                } else {
-                    array_rating_healthy[j] -= amount_rating_healthy;
-                    amount_rating_healthy = 0;
-                }
-            } else {
-                if (amount_rating_healthy + max_subtraction(array_rating_healthy[j], killjoy_rating) > 0) {
-                    amount_rating_healthy += max_subtraction(array_rating_healthy[j], killjoy_rating);
-                    array_rating_healthy[j] += max_subtraction(array_rating_healthy[j], killjoy_rating);
-                } else {
-                    array_rating_healthy[j] -= amount_rating_healthy;
-                    amount_rating_healthy = 0;
-                }
-            }
-            ++j;
-        }
-
-        while (array_rating_healthy.size() != j) {
-            array_rating_infected.push_back(0);
-            amount_rating_infected += killjoy_rating - array_rating_healthy[j];
-            array_rating_healthy.erase(array_rating_healthy.begin() + (long int)(j));
-        }
-    }
+    if (amount_rating_healthy == 0) array_rating_healthy.clear();
 }
 
 int GetNumberOfSteps(std::vector<int> &array_rating_healthy, std::vector<int> &array_rating_infected, int killjoy_rating) {
@@ -84,11 +35,11 @@ int GetNumberOfSteps(std::vector<int> &array_rating_healthy, std::vector<int> &a
 
     if (array_rating_healthy.size() == 0)
         result = 0;
-    else if (array_rating_healthy.size() <= array_rating_infected.size())
+    else if (array_rating_infected.size() > 0)
         result = 1;
     else {
         GetArrayHealthyAccounts(array_rating_healthy, array_rating_infected, killjoy_rating, amount_rating_infected, array_rating_infected.size());
-        
+
         if (array_rating_healthy.size() == 0)
             result = 1;
         else
